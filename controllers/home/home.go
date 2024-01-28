d04ffb9b7f3d21ee8cd0285e13e69ac5b02d7ef5
matthew tirtawidjaja <matthew@tirtagt.xyz>
2024-01-28 22:48:05 +0700

    Sinkronisasi design dari katon ke branch matthew (#3)
    * implementasi awal frontend
    * Luruskan about, dan tambah pemisah (#2)
    * Hapus file tak berguna dan bekas uji coba matthew
    
    ---------
    
    Co-authored-by: tonkaton <katonkurnia28@gmail.com>

--- controllers/home/home.go
+++ controllers/home/home.go
@@ -6,10 +6,10 @@ import (
 	"github.com/TIRTAGT/PD2-Golang.FinalProject/server/controller/utility"
 )
 
-func GET(w http.ResponseWriter, r *http.Request) (*string) {
+func GET(w http.ResponseWriter, r *http.Request) *string {
 	// Siapkan view
-	konten_halaman := utility.FView("/example.html", []utility.VariablePair{
-		{ Key: "name", Value: "Katon", },
+	konten_halaman := utility.FView("/index.html", []utility.VariablePair{
+		{Key: "name", Value: "Katon"},
 	})
 
 	// Kirim konten halaman ke browser (sebagai response dari server)
